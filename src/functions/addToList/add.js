58ed20d8180ca104542c194f326eac867d724359
vik-wed <wedel.viktoria@gmail.com>
2024-10-24 16:17:22 -0400

    Update add test

--- src/functions/addToList/add.js
+++ src/functions/addToList/add.js
@@ -1,5 +1,7 @@
 function add(item, previous) {
-  return [...previous, item];
+  const index = previous.findIndex((listItem) => listItem.toDo === item);
+  if (index !== -1 || item.length === 0) return previous;
+  return [...previous, { toDo: `${item}`, isCompleted: false }];
 }
 
 export { add };
--- src/functions/addToList/add.test.js
+++ src/functions/addToList/add.test.js
@@ -2,12 +2,56 @@ const { add } = require("./add");
 
 describe("Add to to-do list", () => {
   test("it adds an item to the list", () => {
-    const initialList = ["Buy groceries"];
+    const initialList = [{ toDo: "Buy groceries", isCompleted: false }];
     const newItem = "Walk the dog";
     const updatedList = add(newItem, initialList);
 
-    expect(updatedList).toContain("Buy groceries");
-    expect(updatedList).toContain("Walk the dog");
+    expect(updatedList).toEqual([
+      { toDo: "Buy groceries", isCompleted: false },
+      { toDo: "Walk the dog", isCompleted: false },
+    ]);
     expect(updatedList.length).toBe(2);
+  });
+
+  test("it does add a non-string value", () => {
+    const initialList = [{ toDo: "Buy groceries", isCompleted: false }];
+    const newItem = 12345;
+    const updatedList = add(newItem, initialList);
+
+    expect(updatedList).toEqual([
+      { toDo: "Buy groceries", isCompleted: false },
+      { toDo: "12345", isCompleted: false },
+    ]);
+    expect(updatedList.length).toBe(2);
+  });
+
+  test("it adds an item to an empty list", () => {
+    const initialList = [];
+    const newItem = "Do laundry";
+    const updatedList = add(newItem, initialList);
+
+    expect(updatedList).toEqual([{ toDo: "Do laundry", isCompleted: false }]);
+    expect(updatedList.length).toBe(1);
+  });
+
+  test("it does not add a duplicate item to the list", () => {
+    const initialList = [
+      { toDo: "Buy groceries", isCompleted: false },
+      { toDo: "Walk the dog", isCompleted: false },
+    ];
+    const newItem = "Buy groceries";
+    const updatedList = add(newItem, initialList);
+
+    expect(updatedList).toEqual(initialList);
+    expect(updatedList.length).toBe(2);
+  });
+
+  test("it does not add an empty string", () => {
+    const initialList = ["Buy groceries"];
+    const newItem = "";
+    const updatedList = add(newItem, initialList);
+
+    expect(updatedList).toEqual(initialList);
+    expect(updatedList.length).toBe(1);
   });
 });
