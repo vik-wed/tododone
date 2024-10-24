9e3537691aef16002aa28918d664a59381f035f9
vik-wed <wedel.viktoria@gmail.com>
2024-10-24 16:23:46 -0400

    Update remove functionality

--- src/functions/removeFromList/remove.js
+++ src/functions/removeFromList/remove.js
@@ -1,5 +1,5 @@
 function remove(item, previous) {
-  const index = previous.indexOf(item);
+  const index = previous.findIndex((listItem) => listItem.toDo === item);
   if (index !== -1) {
     return [...previous.slice(0, index), ...previous.slice(index + 1)];
   } else {
--- src/functions/removeFromList/remove.test.js
+++ src/functions/removeFromList/remove.test.js
@@ -3,34 +3,47 @@ const { remove } = require("./remove");
 describe("Remove from to-do list", () => {
   test("it removes an item from the list", () => {
     const initialList = [
-      "Buy groceries",
-      "Walk the dog",
-      "Get a haircut",
-      "Code",
+      { toDo: "Buy groceries", isCompleted: false },
+      { toDo: "Walk the dog", isCompleted: false },
+      { toDo: "Get a haircut", isCompleted: false },
+      { toDo: "Code", isCompleted: false },
     ];
     const toBeDeleted = "Walk the dog";
     const updatedList = remove(toBeDeleted, initialList);
 
-    expect(updatedList).toContain("Buy groceries");
-    expect(updatedList).toContain("Get a haircut");
-    expect(updatedList).toContain("Code");
+    expect(updatedList).toEqual([
+      { toDo: "Buy groceries", isCompleted: false },
+      { toDo: "Get a haircut", isCompleted: false },
+      { toDo: "Code", isCompleted: false },
+    ]);
     expect(updatedList.length).toBe(3);
-    expect(updatedList).not.toContain("Walk the dog");
   });
+
   test("if element not in list it returns list", () => {
     const initialList = [
-      "Buy groceries",
-      "Walk the dog",
-      "Get a haircut",
-      "Code",
+      { toDo: "Buy groceries", isCompleted: false },
+      { toDo: "Walk the dog", isCompleted: false },
+      { toDo: "Get a haircut", isCompleted: false },
+      { toDo: "Code", isCompleted: false },
     ];
     const toBeDeleted = "Hello World";
     const updatedList = remove(toBeDeleted, initialList);
 
-    expect(updatedList).toContain("Buy groceries");
-    expect(updatedList).toContain("Get a haircut");
-    expect(updatedList).toContain("Code");
-    expect(updatedList).toContain("Walk the dog");
+    expect(updatedList).toEqual([
+      { toDo: "Buy groceries", isCompleted: false },
+      { toDo: "Walk the dog", isCompleted: false },
+      { toDo: "Get a haircut", isCompleted: false },
+      { toDo: "Code", isCompleted: false },
+    ]);
     expect(updatedList.length).toBe(4);
+  });
+
+  test("it handles empty list", () => {
+    const initialList = [];
+    const toBeDeleted = "Walk the dog";
+    const updatedList = remove(toBeDeleted, initialList);
+
+    expect(updatedList).toEqual([]);
+    expect(updatedList.length).toBe(0);
   });
 });
