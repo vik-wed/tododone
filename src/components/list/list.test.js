a3acdbde0c9a9668c106d3a97bb1f1d413cb6978
vik-wed <wedel.viktoria@gmail.com>
2024-10-25 16:03:35 -0400

    Change list test

--- src/components/list/list.test.js
+++ src/components/list/list.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import List from "./list";
 
-describe("To Do List", () => {
+describe("list", () => {
   test("renders unordered list", () => {
     render(<List />);
     const list = screen.getByRole("list");
