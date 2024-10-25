ab89bbb7f004987685255bfc6d19c6ef2e8dd803
vik-wed <wedel.viktoria@gmail.com>
2024-10-25 16:03:20 -0400

    Update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import "./App.css";
+import Input from "./components/input/input";
+import Button from "./components/button/button";
+import { useState } from "react";
 
 function App() {
+  const [toDoInput, setToDoInput] = useState("");
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">To Do - Done</h1>
       </header>
       <main>
-        <side></side>
+        <div>
+          <Input
+            type="text"
+            value={toDoInput}
+            onChange={(e) => setToDoInput(e.target.value)}
+          />
+          <Button
+            buttonText={"✔"}
+            variant={"add-variant"}
+            isDisabled={toDoInput.length === 0}
+          />
+        </div>
+        <side>
+          <h2>To Do</h2>
+        </side>
+        <side>
+          <h2>Done</h2>
+        </side>
       </main>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders heading", () => {
-  render(<App />);
-  const heading = screen.getByText(/To Do - Done/);
-  expect(heading).toBeInTheDocument();
+describe("Homepage", () => {
+  test("renders heading", () => {
+    render(<App />);
+    const heading = screen.getByText(/To Do - Done/);
+    expect(heading).toBeInTheDocument();
+  });
+  test("renders input", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
+  });
+  test("renders button", () => {
+    render(<App />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+  test("renders to Do List heading", () => {
+    render(<App />);
+    const heading = screen.getAllByRole("heading", { level: 2 });
+    expect(heading.length).toBe(2);
+  });
 });
